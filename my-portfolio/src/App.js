90d581899556f818f9e36636fa1723becfe14d4b
Gabriel Castro <gabe571@Gabriels-MacBook-Pro.local>
2022-08-28 13:38:39 -0700

    navbar hamburger-mobile

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,20 +1,12 @@
-import React from 'react';
-import About from './components/About'
-import Contact from "./components/Contact";
+import React from "react";
 import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
-import Testimonials from "./components/Testimonials";
 
-export default function App() {
+function App() {
   return (
-    <main>
+    <div>
       <Navbar />
-      <About />
-      <Projects />
-      <Skills />
-      <Testimonials />
-      <Contact />
-    </main>
-  );
+    </div>
+  )
 }
+
+export default App;
---
+++ my-portfolio/src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import { FaBars, Fatimes } from 'react-icons/fa'
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
+    return (
+        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+            <div>
+                <h1> GC </h1>
+            </div>
+            {/* menu */}
+            <ul className='hidden md:flex'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Porjects</li>
+                <li>Contact</li>
+            </ul>
+            {/* hamburger */}
+            <div onClick={handleClick} className='md:hidden z-10'>
+                <FaBars />
+            </div>
+            {/* mobile menu */}
+            <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+                <li className='py-6 text-4xl'>Home</li>
+                <li className='py-6 text-4xl'>About</li>
+                <li className='py-6 text-4xl'>Skills</li>
+                <li className='py-6 text-4xl'>Porjects</li>
+                <li className='py-6 text-4xl'>Contact</li>
+            </ul>
+            {/* Social icons */}
+            <div className='hidden'></div>
+        </div >
+    )
+}
+
+export default Navbar
