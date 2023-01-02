6348c26c379a24ef1e6e4f41bd14fd4b47be8166
Gabriel Castro <gabe571@Gabriels-MacBook-Pro-2.local>
2023-01-02 13:59:53 -0800

    about page

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import About from "./components/About"
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <About />
     </div>
   )
 }
