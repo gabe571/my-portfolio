3f2866fed6a71d6cd0f6b67811b35c612ba1d2f6
Gabriel Castro <gabe571@Gabriels-MacBook-Pro-2.local>
2023-01-02 13:34:39 -0800

    styling

--- my-portfolio/src/components/Home.jsx
+++ my-portfolio/src/components/Home.jsx
@@ -7,11 +7,20 @@ const Home = () => {
 
             {/* Container */}
             <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-                <p> Hi, my name is</p>
-                <h1>Gabriel Castro</h1>
-                <h2> I am a full-stack web developer</h2>
+                <p className='text-red-600'> Hi, my name is</p>
+                <h1 className='text-4xl sm:text-5xl font-bold text-white'>Gabriel Castro</h1>
+                <h2 className='text-4xl sm:text-5xl font-bold text-gray-500'> I am a full-stack web developer.</h2>
+                <p className='text-gray-500 py-4 max-w-[700px]'> I am a full-stack developer, with a focus on building responsive quality web applications.</p>
+                <div>
+                    <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-red-600 hover:border-red-600'>
+                        View Work
+                        <span className='group-hover:rotate-90 duration-300'>
+                            <HiArrowNarrowRight className='ml-3' />
+                        </span>
+                    </button>
+                </div>
             </div>
-        </div>
+        </div >
     )
 }
 
