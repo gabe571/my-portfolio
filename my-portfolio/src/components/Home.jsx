f6a3b99baed1ce812b458a0201b9361158faed8f
Gabriel Castro <gabe571@Gabriels-MacBook-Pro-2.local>
2023-01-02 13:02:07 -0800

    home profile

--- my-portfolio/src/components/Home.jsx
+++ my-portfolio/src/components/Home.jsx
@@ -3,8 +3,14 @@ import { HiArrowNarrowRight } from 'react-icons/hi'
 
 const Home = () => {
     return (
-        <div className='w-full h-screen bg-[#0a192f]'>
+        <div name='home' className='w-full h-screen bg-[#0a192f]'>
 
+            {/* Container */}
+            <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+                <p> Hi, my name is</p>
+                <h1>Gabriel Castro</h1>
+                <h2> I am a full-stack web developer</h2>
+            </div>
         </div>
     )
 }
