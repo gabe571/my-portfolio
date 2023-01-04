e97e3badadbb6fefd80831f71750263c16894760
Gabriel Castro <gabe571@Gabriels-MacBook-Pro-2.local>
2023-01-04 15:37:59 -0800

    pb resolved

--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
-import PixtureBox from '../assests/projects/pixturebox.png'
+
+
+import pixturebox from '../assests/projects/pixturebox.png'
 
 const Projects = () => {
     return (
@@ -10,25 +12,30 @@ const Projects = () => {
                     <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-red-600'>Projects</p>
                     <p className='py-6'>// Checkout some of my recent projects</p>
                 </div>
-                <div style={{ backgroundImage: `url(${PixtureBox})` }}>
-                    <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-                        <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                        </div>
-                    </div>
-                    <span>
+                <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
 
-                    </span>
-                    <div>
-                        <a href='/'>
-                            <button></button>
-                        </a>
-                        <a href='/'>
-                            <button></button>
-                        </a>
+                    <div style={{ backgroundImage: `url(${pixturebox})` }}>
+                        <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                            <div className='opacity-0 group-hover:opacity-100'>
+                                <span className='text-2xl font-bold text-white tracking-wider'>
+                                    React JS App
+                                </span>
+                                <div className='pt-8 text-center'>
+                                    <a href='/'>
+                                        <button className="text-center rounded-lg px-4 py-3 m-2
+                                         bg-white text-gray-700 font-bold text-lg">Demo</button>
+                                    </a>
+                                    <a href='/'>
+                                        <button className="text-center rounded-lg px-4 py-3 m-2
+                                         bg-white text-gray-700 font-bold text-lg">Code</button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
