1862d0dcb4d505ac43d1830150527d9a37049096
Gabriel Castro <gabe571@Gabriels-MacBook-Pro-2.local>
2023-01-05 14:40:31 -0800

    react smooth scroll

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook } from 'react-icons/f
 import { HiOutlineMail } from 'react-icons/hi'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
 import Home from './Home'
+import { Link } from 'react-scroll'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false)
@@ -14,11 +15,31 @@ const Navbar = () => {
             </div>
             {/* menu */}
             <ul className='hidden md:flex'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Projects</li>
-                <li>Contact</li>
+                <li>
+                    <Link to="home" smooth={true} duration={500}>
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to="about" smooth={true} duration={500}>
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link to="technologies" smooth={true} duration={500}>
+                        Technologies
+                    </Link>
+                </li>
+                <li>
+                    <Link to="projects" smooth={true} duration={500}>
+                        Projects
+                    </Link>
+                </li>
+                <li>
+                    <Link to="contact" smooth={true} duration={500}>
+                        Contact
+                    </Link>
+                </li>
             </ul>
             {/* hamburger */}
             <div onClick={handleClick} className='md:hidden z-10'>
@@ -26,11 +47,27 @@ const Navbar = () => {
             </div>
             {/* mobile menu */}
             <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-                <li className='py-6 text-4xl'>Home</li>
-                <li className='py-6 text-4xl'>About</li>
-                <li className='py-6 text-4xl'>Skills</li>
-                <li className='py-6 text-4xl'>Projects</li>
-                <li className='py-6 text-4xl'>Contact</li>
+                <li className='py-6 text-4xl'>
+                    <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+                        Home
+                    </Link></li>
+                <li className='py-6 text-4xl'>
+                    <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+                        About
+                    </Link>
+                </li>
+                <li className='py-6 text-4xl'>
+                    <Link onClick={handleClick} to="technologies" smooth={true} duration={500}>
+                        Technologies
+                    </Link></li>
+                <li className='py-6 text-4xl'>
+                    <Link onClick={handleClick} to="projects" smooth={true} duration={500}>
+                        Projects
+                    </Link></li>
+                <li className='py-6 text-4xl'>
+                    <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+                        Contact
+                    </Link></li>
             </ul>
             {/* Social icons */}
             <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
--- my-portfolio/src/components/TechStack.jsx
+++ my-portfolio/src/components/TechStack.jsx
@@ -8,7 +8,7 @@ import RUBYONRAILS from '../assests/rubyonrails.png'
 
 const Skills = () => {
     return (
-        <div name='skills' className=' w-full h-screen bg-[#0a192f] text-gray-300'>
+        <div name='technologies' className=' w-full h-screen bg-[#0a192f] text-gray-300'>
             <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
                 <div>
                     <p className='text-4xl font-bold inline border-b-4 border-red-600'> Tech Stack </p>
